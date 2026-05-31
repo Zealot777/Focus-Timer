f182f9f2b0222be335e5e8311ed53c7b82f11230
Zealot777 <zealot000777@gmail.com>
2026-06-01 02:25:37 +0900

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 //api.js
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchSubjects = async () => {
   const res = await fetch(`${BASE_URL}/subjects`);
